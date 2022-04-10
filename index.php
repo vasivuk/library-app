317eda49f039603647ed29a56819e3f7eaab5e6c
vasivuk <vasivuk@yahoo.com>
2022-04-10 17:28:29 +0200

    Added login page, changed files to .php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+<body>
+    <div class="login-container">
+        <form action="main.php" method="POST">
+            <label for="username">Username:</label>
+            <input
+                type="text"
+                name="username"
+                id="username"
+            >
+
+            <label for="password">Password:</label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+            >
+
+            <button type="">Login</button>
+        </form>
+    </div>
+</body>
+</html>
